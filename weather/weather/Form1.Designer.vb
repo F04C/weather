7c8bde6b873f471666e00d54e31a0f86b2175ec7
F04C <israeldarrel@yahoo.com>
2023-10-09 15:22:55 +0800

    added

--- weather/weather/Form1.Designer.vb
+++ weather/weather/Form1.Designer.vb
@@ -22,10 +22,138 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Panel7 = New System.Windows.Forms.Panel()
+        Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.GetWeatherButton = New System.Windows.Forms.Button()
+        Me.CityTextBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Panel4.SuspendLayout()
+        Me.Panel6.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(640, 35)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel2.Location = New System.Drawing.Point(0, 35)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(90, 605)
+        Me.Panel2.TabIndex = 1
+        '
+        'Panel3
+        '
+        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel3.Location = New System.Drawing.Point(550, 35)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(90, 605)
+        Me.Panel3.TabIndex = 2
+        '
+        'Panel4
+        '
+        Me.Panel4.Controls.Add(Me.Panel7)
+        Me.Panel4.Controls.Add(Me.Panel6)
+        Me.Panel4.Controls.Add(Me.Panel5)
+        Me.Panel4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel4.Location = New System.Drawing.Point(90, 35)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(460, 605)
+        Me.Panel4.TabIndex = 4
+        '
+        'Panel7
+        '
+        Me.Panel7.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel7.Location = New System.Drawing.Point(0, 495)
+        Me.Panel7.Name = "Panel7"
+        Me.Panel7.Size = New System.Drawing.Size(460, 65)
+        Me.Panel7.TabIndex = 3
+        '
+        'Panel6
+        '
+        Me.Panel6.Controls.Add(Me.GetWeatherButton)
+        Me.Panel6.Controls.Add(Me.CityTextBox)
+        Me.Panel6.Controls.Add(Me.Label1)
+        Me.Panel6.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel6.Location = New System.Drawing.Point(0, 65)
+        Me.Panel6.Name = "Panel6"
+        Me.Panel6.Size = New System.Drawing.Size(460, 430)
+        Me.Panel6.TabIndex = 2
+        '
+        'GetWeatherButton
+        '
+        Me.GetWeatherButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GetWeatherButton.Location = New System.Drawing.Point(169, 136)
+        Me.GetWeatherButton.Name = "GetWeatherButton"
+        Me.GetWeatherButton.Size = New System.Drawing.Size(143, 36)
+        Me.GetWeatherButton.TabIndex = 2
+        Me.GetWeatherButton.Text = "Search"
+        Me.GetWeatherButton.UseVisualStyleBackColor = True
+        '
+        'CityTextBox
+        '
+        Me.CityTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CityTextBox.Location = New System.Drawing.Point(96, 59)
+        Me.CityTextBox.Name = "CityTextBox"
+        Me.CityTextBox.Size = New System.Drawing.Size(278, 38)
+        Me.CityTextBox.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(163, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(149, 31)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter City: "
+        '
+        'Panel5
+        '
+        Me.Panel5.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel5.Location = New System.Drawing.Point(0, 0)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(460, 65)
+        Me.Panel5.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(640, 640)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel6.ResumeLayout(False)
+        Me.Panel6.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Panel7 As Panel
+    Friend WithEvents Panel6 As Panel
+    Friend WithEvents GetWeatherButton As Button
+    Friend WithEvents CityTextBox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel5 As Panel
 End Class
--- weather/weather/Form1.vb
+++ weather/weather/Form1.vb
@@ -1,3 +1,30 @@
-﻿Public Class Form1
+﻿Imports System.Net.Http
+Imports Newtonsoft.Json.Linq
 
+Public Class Form1
+    Private Async Sub GetWeatherButton_Click(sender As Object, e As EventArgs) Handles GetWeatherButton.Click
+        Dim city As String = CityTextBox.Text.Trim()
+        Dim apiKey As String = "YOUR_API_KEY" ' Replace with your OpenWeatherMap API key
+
+        If Not String.IsNullOrWhiteSpace(city) Then
+            Try
+                Dim httpClient As New HttpClient()
+                Dim response As HttpResponseMessage = Await httpClient.GetAsync($"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric")
+                Dim jsonResponse As String = Await response.Content.ReadAsStringAsync()
+                Dim data As JObject = JObject.Parse(jsonResponse)
+
+                If response.IsSuccessStatusCode AndAlso data("cod").ToString() = "200" Then
+                    Dim weatherDesc As String = data("weather")(0)("description").ToString()
+                    Dim temperature As Double = data("main")("temp").ToObject(Of Double)()
+                    WeatherLabel.Text = $"Weather: {weatherDesc}{Environment.NewLine}Temperature: {temperature}°C"
+                Else
+                    WeatherLabel.Text = "City not found."
+                End If
+            Catch ex As Exception
+                WeatherLabel.Text = "An error occurred."
+            End Try
+        Else
+            WeatherLabel.Text = "Please enter a city."
+        End If
+    End Sub
 End Class
